2184f35372421759e6d7a3f681cb10b9811aeb9a
Roger <49531890+rogergibertrovira@users.noreply.github.com>
2023-05-24 10:31:53 +0200

    Components 7

--- farfaraway/src/app/maps/maps.component.ts
+++ farfaraway/src/app/maps/maps.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
-  styleUrls: ['./maps.component.css']
+  styleUrls: ['./maps.component.css'],
 })
 export class MapsComponent {
-
+  zoom = 12;
+  center: google.maps.LatLngLiteral = { lat: 41.3851, lng: 2.1734 };
 }
